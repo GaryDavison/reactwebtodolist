7354d8ed2e45fe9216a8e1a3b64d048cb717e9ab
Gary Davison <gary.davison@jhu.edu>
2020-07-15 12:53:18 -0400

    Create index.html index.js index.css.  Initial commit.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// index.js
+// source: https://www.kirupa.com/react/simple_todo_app_react.htm
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <p>Hello!</p>
+    </div>,
+    destination
+);
